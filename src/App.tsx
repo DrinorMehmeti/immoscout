54c0cbe05eff974cdca2b31f5b3cf4b10826f6e5
DrinorMehmeti <91284926+DrinorMehmeti@users.noreply.github.com>
2025-06-02 00:19:49 +0200

    Fix Navigate component URL error

--- src/App.tsx
+++ src/App.tsx
@@ -294,7 +294,7 @@ function App() {
           <Route path="/admin/settings" element={<AdminRoute><AdminSettings /></AdminRoute>} />
           
           {/* Fallback */}
-          <Route path="*" element={<Navigate to="/\" replace />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
       
